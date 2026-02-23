a7169ef2db4b39801bf1af13edc496a3d4edde09
Nishantkalia1992 <nishantkalia71001@gmail.com>
2026-02-23 15:57:21 +0530

    add registry server

--- ServiceRegistry/src/test/java/com/serviceRegistry/ServiceRegistryApplicationTests.java
+++ ServiceRegistry/src/test/java/com/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.discovery;
+package com.serviceRegistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DescoveryServerApplicationTests {
+class ServiceRegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
