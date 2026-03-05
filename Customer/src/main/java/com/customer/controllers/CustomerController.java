03120e1d77fd57c75fc31eb1cb804831e10b06a9
Nishantkalia1992 <nishantkalia71001@gmail.com>
2026-03-05 23:58:45 +0530

    Update CustomerController.java

--- Customer/src/main/java/com/customer/controllers/CustomerController.java
+++ Customer/src/main/java/com/customer/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package com.customer.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth/customer")
+@CrossOrigin(origins = "*")
 public class CustomerController {
 	@Autowired
 	CustomerServices customerServices;
