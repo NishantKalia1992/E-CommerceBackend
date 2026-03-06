a75f7a7a668458226e3eabeeaa23be177d666342
Nishantkalia1992 <nishantkalia71001@gmail.com>
2026-03-06 12:04:46 +0530

    Update OrderController.java

--- Order/src/main/java/com/order/controllers/OrderController.java
+++ Order/src/main/java/com/order/controllers/OrderController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class OrderController {
 	
 	final OrderService orderService;
